53b3574852d77f0a6169c072b1c222b480618ab9
RickyZhou44 <819894286@qq.com>
2019-03-01 23:02:49 +0800

    Add files via upload

---
+++ insurancesys/src/main/java/whu/iss/insurancesys/InsurancesysApplication.java
@@ -0,0 +1,13 @@
+package whu.iss.insurancesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancesysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsurancesysApplication.class, args);
+    }
+
+}
