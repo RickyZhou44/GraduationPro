c846e278bff7855584c85ba9d11fc77bb4182586
Johnsoncoco <3219716361@qq.com>
2019-03-11 13:36:07 +0800

    set cors

--- insurancesys/src/main/java/whu/iss/insurancesys/control/Test.java
+++ insurancesys/src/main/java/whu/iss/insurancesys/control/Test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://127.0.0.1:80", maxAge = 3600)
+@CrossOrigin(origins = "*")
 @RestController
 public class Test {
 
