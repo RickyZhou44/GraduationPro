2300d64bf964a16cb45def2e72e51ecaae83d0b4
jpz <1205448839@qq.com>
2019-03-25 14:38:23 +0800

    个人绩效预警

--- insurancesys/src/main/java/whu/iss/insurancesys/controller/PersonalPerformanceWarning.java
+++ insurancesys/src/main/java/whu/iss/insurancesys/controller/PersonalPerformanceWarning.java
@@ -1,6 +1,7 @@
 package whu.iss.insurancesys.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.ibatis.annotations.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,11 @@ import whu.iss.insurancesys.entity.PersonalPerformanceWaringObjectResult;
 import whu.iss.insurancesys.service.PersonalPerformanceWarningService;
 
 import javax.annotation.Resource;
+import java.util.List;
 
+/**
+ * 个人绩效预警
+ */
 @RestController
 @RequestMapping("/api")
 public class PersonalPerformanceWarning {
@@ -26,7 +31,7 @@ public class PersonalPerformanceWarning {
     private String performanceWarnPeople(@RequestParam(value = "name") String name, @RequestParam(value = "number") String number, @RequestParam(value = "idCard") String idCard, @RequestParam(value = "branch[]", required = false) String[] branchs) {
         logger.debug("name:{},number:{},idCard:{},branch:{}\n", name, number, idCard, branchs);
         ResultInfo resultInfo = new ResultInfo();
-        PersonalPerformanceWaringObjectResult data = personalPerformanceWarningService.getPeopleInfo(name, Long.parseLong(number), idCard, branchs);
+        List<PersonalPerformanceWaringObjectResult> data = personalPerformanceWarningService.getPeopleInfo(name, Long.parseLong(number), idCard, branchs);
         if (data != null) {
             resultInfo.setResult(true);
             resultInfo.setData(data);
@@ -34,5 +39,13 @@ public class PersonalPerformanceWarning {
         String res = JSONObject.toJSONString(resultInfo);
         logger.debug("查询结果：{}", res);
         return res;
+    }
+
+
+    @GetMapping("/performanceWarn")
+    private String performanceWarn(@RequestParam(value = "name") String name, @RequestParam(value = "number") String number, @RequestParam(value = "idCard") String idCard, @RequestParam(value = "baseMonth") String baseMonth, @RequestParam("project") String project, @RequestParam("category") String category, @RequestParam("objRank") String rank, @RequestParam("period") String period) {
+        logger.debug("name:{}\nnumber:{}\nidCard:{}\nbaseMonth:{}\nproject:{}\ncategory:{}\nobjRank:{}\nperiod:{}\n", name, number, idCard, baseMonth, project, category, rank, period);
+        ResultInfo resultInfo = new ResultInfo();
+        return null;
     }
 }
--- insurancesys/src/main/java/whu/iss/insurancesys/service/impl/PersonalPerformanceWarningServiceImpl.java
+++ insurancesys/src/main/java/whu/iss/insurancesys/service/impl/PersonalPerformanceWarningServiceImpl.java
@@ -1,20 +1,28 @@
 package whu.iss.insurancesys.service.impl;
 
 import org.springframework.stereotype.Service;
-import whu.iss.insurancesys.dao.PersonalPerformanceWaringObjectResultDao;
+import whu.iss.insurancesys.dao.PersonalPerformanceWaringDao;
 import whu.iss.insurancesys.entity.PersonalPerformanceWaringObjectResult;
+import whu.iss.insurancesys.entity.PersonalPerformanceWarningQueryResult;
 import whu.iss.insurancesys.service.PersonalPerformanceWarningService;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class PersonalPerformanceWarningServiceImpl implements PersonalPerformanceWarningService {
 
     @Resource
-    private PersonalPerformanceWaringObjectResultDao personalPerformanceWaringObjectResultDao;
+    private PersonalPerformanceWaringDao personalPerformanceWaringDao;
 
     @Override
-    public PersonalPerformanceWaringObjectResult getPeopleInfo(String name, long number, String idCard, String[] branchs) {
-        return personalPerformanceWaringObjectResultDao.findAll(name, number, idCard, branchs);
+    public List<PersonalPerformanceWaringObjectResult> getPeopleInfo(String name, long number, String idCard, String[] branchs) {
+        return personalPerformanceWaringDao.findPersonInfo(name, number, idCard, branchs);
+    }
+
+    @Override
+    public PersonalPerformanceWarningQueryResult query(long number, String baseMonth, String project, String category, String period) {
+        PersonalPerformanceWarningQueryResult result = personalPerformanceWaringDao.findOne(number);
+        return null;
     }
 }
