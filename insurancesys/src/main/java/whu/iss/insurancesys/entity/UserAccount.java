4f094946b3199171460b655cfb590a782bdce518
Sean She <327583303@qq.com>
2019-03-30 22:50:16 +0800

    111

---
+++ insurancesys/src/main/java/whu/iss/insurancesys/entity/UserAccount.java
@@ -0,0 +1,102 @@
+package whu.iss.insurancesys.entity;
+
+/*
+ * @Author SeanShe
+ * @Date 3/30/2019 12:19
+ * Notes:
+ * */
+
+import java.util.Date;
+
+public class UserAccount {
+
+	private int userNo;
+
+	private String userType;
+
+	private String username;
+
+	private String userRealname;
+
+	private String email;
+
+	private String password;
+
+	private Date starttime;
+
+	private Date endtime;
+
+	private String accountStatus;
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserRealname() {
+		return userRealname;
+	}
+
+	public void setUserRealname(String userRealname) {
+		this.userRealname = userRealname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getStarttime() {
+		return starttime;
+	}
+
+	public void setStarttime(Date starttime) {
+		this.starttime = starttime;
+	}
+
+	public Date getEndtime() {
+		return endtime;
+	}
+
+	public void setEndtime(Date endtime) {
+		this.endtime = endtime;
+	}
+
+	public String getAccountStatus() {
+		return accountStatus;
+	}
+
+	public void setAccountStatus(String accountStatus) {
+		this.accountStatus = accountStatus;
+	}
+}
