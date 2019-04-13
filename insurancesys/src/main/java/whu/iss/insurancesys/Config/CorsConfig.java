dfc63d75fe63e896f56ac704650cb7730e21e1c7
RickZhou <819894286@qq.com>
2019-04-14 00:05:26 +0800

    1111`

--- insurancesys/src/main/java/whu/iss/insurancesys/Config/CorsConfig.java
+++ insurancesys/src/main/java/whu/iss/insurancesys/Config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:8080")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "DELETE", "PUT","PATCH")
                 .maxAge(3600);
